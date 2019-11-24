a733c45229bd3b1d6cd9080733cf8228949fe5e3
SSaquif <ssaquif@gmail.com>
2019-11-24 14:51:10 -0500

    created boilerplate for listgroup component

---
+++ src/components/common/listGroup.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+//Using SFS
+const ListGroup = () => {
+	return (
+		<React.Fragment>
+			{' '}
+			<ul className="list-group">
+				<li className="list-group-item"></li>
+			</ul>{' '}
+		</React.Fragment>
+	);
+};
+
+export default ListGroup;
