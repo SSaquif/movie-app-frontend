489f3152fcdf0f66a6d047418da426390a9c81d5
SSaquif <ssaquif@gmail.com>
2019-12-24 03:39:30 -0500

    Movies component updated with genre selection and related changes to pagination etc

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import { getMovies, deleteMovie } from '../services/helperMovieDB';
+import { getGenres } from '../services/helperGenreDB';
 import Like from './common/like';
 import Pagination from './common/pagination';
+import ListGroup from './common/listGroup';
 import { paginate } from '../utils/paginate';
 
 class Movies extends Component {
 	state = {
-		movies: getMovies(), //this is not the right way to initilize states, for now its ok
+		//moies and genres will be received from backend later
+		//Right place to inititalise them is componentDidMount() method/LCH
+		movies: [],
+		genres: [],
 		pageSize: 4,
-		currentPage: 1
+		currentPage: 1,
+		selectedGenre: ''
 	};
+
+	//This method is called when an instance of the component is rendered in the DOM
+	componentDidMount() {
+		//get genre returns an array of objects with 2 properties, id and name
+		const genres = [{ _id: 'all', name: 'All Genres' }, ...getGenres()];
+		//this.setState({ movies: getMovies(), genres: genres });
+		this.setState({ movies: getMovies(), genres }); //equivalent to the above
+	}
 
 	handleLike = (movie) => {
 		console.log(movie.title);
@@ -57,19 +71,40 @@ class Movies extends Component {
 		this.setState({ movies: updatedMovies });
 	};
 
+	handleGenreSelect = (genre) => {
+		console.log(genre);
+		//Remeber when state changes, The component and all its children are re rendered
+		//upon clicking (in child component listGroup) selected genre will be the one clicked
+		//remember to setcurrentpage back to 1 when we select a new genre
+		this.setState({ selectedGenre: genre, currentPage: 1 });
+	};
+
 	render() {
+		//No longer need count using filteredMovies' length instead
 		//TODO:Use object destructuring everywhere to clean up code(currenmtly not)
 		//TODO:Will not have to do this.state everytime (seems like convention to use destructuring in REACT)
-		const { length: count1 } = this.state.movies; //OBJECT DESTRUCTURING SYNTAX OF ABOVE
+		//const count = this.state.movies.length;
+		//const { length: count1 } = this.state.movies; //OBJECT DESTRUCTURING SYNTAX OF ABOVE
 
-		const count = this.state.movies.length;
-		const movies = paginate(
-			this.state.movies,
-			this.state.currentPage,
-			this.state.pageSize
-		);
+		const {
+			selectedGenre,
+			currentPage,
+			pageSize,
+			movies: allMovies //renaming movies to allMovies
+		} = this.state;
 
-		if (count === 0) {
+		//remember empty string is falsy
+		const filteredMovies =
+			selectedGenre && selectedGenre.name !== 'All Genres'
+				? allMovies.filter((movie) => {
+						console.log('Selected Gen', selectedGenre.name);
+						return movie.genre.name === selectedGenre.name;
+				  })
+				: allMovies;
+
+		const paginatedMovies = paginate(filteredMovies, currentPage, pageSize);
+
+		if (filteredMovies.length === 0) {
 			return (
 				<p>
 					<b>There are no movies in the database</b>
@@ -78,58 +113,75 @@ class Movies extends Component {
 		}
 		return (
 			<React.Fragment>
-				<p>
-					<b>Showing {count} movies found in database</b>
-				</p>
-				<table className="table">
-					<thead>
-						<tr>
-							<th>Title</th>
-							<th>genre</th>
-							<th>Stock</th>
-							<th>Daily Rate</th>
-							<th></th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						{movies.map((movie) => (
-							<tr key={movie._id}>
-								<td>{movie.title}</td>
-								<td>{movie.genre.name}</td>
-								<td>{movie.numberInStock}</td>
-								<td>{movie.dailyRentalRate}</td>
-								<td>
-									<Like
-										liked={movie.liked}
-										onClick={() => this.handleLike(movie)}
-									/>
-								</td>
-								<td>
-									<button
-										className="btn btn-danger btn-sm"
-										onClick={() => this.handleDelete(movie)}
-									>
-										{' '}
-										Delete
-									</button>
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-				{/*Our Pagination component will need to know the following:: */}
-				{/*Total Number of Items(ie:the movie count)::which we already have)*/}
-				{/*No of items(ie:movies)per page::We should store this in state, cause this can change)*/}
-				{/*Finally, Should raise an event when page users clicks on a page button*/}
-				<Pagination
-					itemCount={count}
-					pageSize={this.state.pageSize}
-					currentPage={this.state.currentPage}
-					onPageChange={this.handlePageChange}
-					onPageChangePrev={this.handlePageChangePrevious}
-					onPageChangeNext={this.handlePageChangeNext}
-				/>
+				<div className="row">
+					<div className="column-left">
+						<ListGroup
+							items={this.state.genres}
+							selectedItem={this.state.selectedGenre}
+							onItemSelect={this.handleGenreSelect}
+							//Now using defaultprops in Component instead (https://reactjs.org/docs/react-component.html near the end for defaultprops)
+							//textProperty="name" //adding this two property props
+							//uniqueKeyProperty="_id" // to make the list component more reusable in other apps
+						></ListGroup>
+					</div>
+					<div className="column-right">
+						<p>
+							<b>Showing {filteredMovies.length} movies found in database</b>
+						</p>
+						<table className="table">
+							<thead>
+								<tr>
+									<th>Title</th>
+									<th>genre</th>
+									<th>Stock</th>
+									<th>Daily Rate</th>
+									<th></th>
+									<th></th>
+								</tr>
+							</thead>
+							<tbody>
+								{paginatedMovies.map((movie) => (
+									<tr key={movie._id}>
+										<td>{movie.title}</td>
+										<td>{movie.genre.name}</td>
+										<td>{movie.numberInStock}</td>
+										<td>{movie.dailyRentalRate}</td>
+										<td>
+											<Like
+												liked={movie.liked}
+												onClick={() => this.handleLike(movie)}
+											/>
+										</td>
+										<td>
+											<button
+												className="btn btn-danger btn-sm"
+												onClick={() => this.handleDelete(movie)}
+											>
+												{' '}
+												Delete
+											</button>
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+						{/*Our Pagination component will need to know the following:: */}
+						{/*Total Number of Items(ie:the movie count)::which we already have)*/}
+						{/*No of items(ie:movies)per page::We should store this in state, cause this can change)*/}
+						{/*Finally, Should raise an event when page users clicks on a page button*/}
+						<Pagination
+							itemCount={filteredMovies.length}
+							pageSize={this.state.pageSize}
+							currentPage={this.state.currentPage}
+							onPageChange={this.handlePageChange}
+							onPageChangePrev={this.handlePageChangePrevious}
+							onPageChangeNext={this.handlePageChangeNext}
+						/>
+					</div>
+				</div>
+				<div className="row">
+					<h1>hello</h1>
+				</div>
 			</React.Fragment>
 		);
 	}
