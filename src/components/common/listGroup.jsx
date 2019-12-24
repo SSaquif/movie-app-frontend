adf05234bd033d0913e1a189eb8d15cdd089230a
SSaquif <ssaquif@gmail.com>
2019-12-24 03:37:06 -0500

    reusable SFC listgroup to be used for listing genres in this app

--- src/components/common/listGroup.jsx
+++ src/components/common/listGroup.jsx
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
 
-//Using SFS
-const ListGroup = () => {
+//Using SFC
+const ListGroup = (props) => {
+	//still need textProperty, uniqueKeyProperty from props. even when using default props instead
+	const {
+		items,
+		textProperty,
+		uniqueKeyProperty,
+		selectedItem,
+		onItemSelect
+	} = props; //obj destructuring
+	//const { items, onItemSelect } = props; //obj destructuring
 	return (
 		<React.Fragment>
 			{' '}
 			<ul className="list-group">
-				<li className="list-group-item"></li>
+				{items.map((item) => {
+					return (
+						//using bracket notation to access properties dynamically
+						<li
+							key={item[uniqueKeyProperty]}
+							onClick={() => onItemSelect(item)}
+							className={
+								item === selectedItem
+									? 'list-group-item active'
+									: 'list-group-item'
+							}
+						>
+							{item[textProperty]}
+						</li>
+					);
+				})}
 			</ul>{' '}
 		</React.Fragment>
 	);
 };
-
+//default props::new property and we set it to an object
+//https://reactjs.org/docs/react-component.html::See Class Properties section
+//Make sure to spell it properly defaultProps
+ListGroup.defaultProps = {
+	//will put this in props
+	textProperty: 'name', //adding this to property props
+	uniqueKeyProperty: '_id' // to make the list component more reusable in other apps
+};
 export default ListGroup;
